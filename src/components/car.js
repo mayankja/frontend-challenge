b42915bc83d5f7735d287ce85e08c0496dd6ae50
mayankja <mayankjadam74@gmail.com>
2020-09-15 18:40:11 +0530

    level 4 commit

--- src/components/car.js
+++ src/components/car.js
@@ -5,7 +5,7 @@ export default ({ availability, brand, id, model, pricePerDay, pricePerKm }) => 
     if (Constants[id]) return (
         <div className="container">
             <div className="row m-5 border">
-                <div className="col p-5">
+                <div className="col-5 p-5">
                     <h5 className="card-title">{brand} {` `} {model}</h5>
                     <p className="card-text">
                         id: {id} <br />
@@ -15,10 +15,14 @@ export default ({ availability, brand, id, model, pricePerDay, pricePerKm }) => 
                         price/Km: {pricePerKm} <br />
                         <br />
                         Total price (per Day): {availability.maxDuration * pricePerDay} <br />
-                        Total price (per KM): {availability.maxDistance * pricePerKm}
+                        Total price (per KM): {availability.maxDistance * pricePerKm} <br />
+                        <br />
+                        Discounted Price/Day after 1 day: {pricePerDay * 0.1} <br />
+                        Discounted Price/Day after 4 day: {pricePerDay * 0.3} <br />
+                        Discounted Price/Day after 10 day: {pricePerDay * 0.5}
                     </p>
                 </div>
-                <div className="col m-0 p-0">
+                <div className="col-7 m-0 p-0">
                     <img src={Constants[id]} alt={`${brand} ${model}`} width="100%" height="100%"></img>
                 </div>
             </div>
